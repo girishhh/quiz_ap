238fd142d14e2863c92cbc34833a5813fd8ad5e9
girishhh <girikulkarni03@gmail.com>
2017-10-20 20:36:37 +0530

    changes

--- app/models/account.rb
+++ app/models/account.rb
@@ -2,4 +2,5 @@ class Account < ActiveRecord::Base
  belongs_to :person
  has_one :account_history	
  validates :account_name,presence: true
+ hbjhbjhb 
 end
--- app/models/account_history.rb
+++ app/models/account_history.rb
@@ -1,3 +1,4 @@
 class AccountHistory < ActiveRecord::Base
+	hbjhbhjbbjhbjhb
 	belongs_to :account
 end
